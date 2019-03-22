16a18fe6f49b4e32167973c79f4d2e5397512b72
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-22 02:01:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    @anagrams_array = []
+    @word = String.new
+  def initialize(word)
+    @word = word
+  end
+  def self.match(input_array)
+    input_array.each do |possible_anagram|
+      if possible_anagram.split("").sort == @word.split("").sort && possible_anagram != @word
+        @anagrams_array << possible_anagram
+      end
+    end
+  end
+end
